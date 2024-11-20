0beeba237a30566dcc4995b7cf7ab9d4b8cc0e7d
Liam <33645555+lj3954@users.noreply.github.com>
2024-11-19 22:09:28 -0800

    refactor: Move alma configuration creation to separate function

--- internal/os/alma.go
+++ internal/os/alma.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"sync"
 
 	"github.com/quickemu-project/quickget_configs/internal/cs"
 )
@@ -34,38 +35,44 @@ func (Alma) CreateConfigs(errs, csErrs chan Failure) ([]Config, error) {
 
 	for release := range releases {
 		for _, arch := range x86_64_aarch64 {
-			mirror := fmt.Sprintf("%s%s/isos/%s/", almaMirror, release, arch)
-			go func() {
-				defer wg.Done()
-
-				page, err := capturePage(mirror)
-				if err != nil {
-					errs <- Failure{Release: release, Arch: arch, Error: err}
-					return
-				}
-				checksums, err := cs.Build(cs.Sha256Regex, mirror+"CHECKSUM")
-				if err != nil {
-					csErrs <- Failure{Release: release, Arch: arch, Error: err}
-				}
-				for _, match := range isoRe.FindAllStringSubmatch(page, -1) {
-					if strings.HasSuffix(match[0], ".manifest") {
-						continue
-					}
-					iso, edition := match[1], match[2]
-					url := mirror + iso
-					checksum := checksums[iso]
-					ch <- Config{
-						Release: release,
-						Edition: edition,
-						Arch:    arch,
-						ISO: []Source{
-							urlChecksumSource(url, checksum),
-						},
-					}
-				}
-			}()
+			go addAlmaConfigs(release, arch, isoRe, ch, wg, errs, csErrs)
 		}
 	}
 
 	return waitForConfigs(ch, wg), nil
+}
+
+func addAlmaConfigs(release string, arch Arch, isoRe *regexp.Regexp, ch chan Config, wg *sync.WaitGroup, errs, csErrs chan Failure) {
+	defer wg.Done()
+
+	mirror := fmt.Sprintf("%s%s/isos/%s/", almaMirror, release, arch)
+	page, err := capturePage(mirror)
+	if err != nil {
+		errs <- Failure{Release: release, Arch: arch, Error: err}
+		return
+	}
+
+	checksums, err := cs.Build(cs.Sha256Regex, mirror+"CHECKSUM")
+	if err != nil {
+		csErrs <- Failure{Release: release, Arch: arch, Error: err}
+		return
+	}
+
+	for _, match := range isoRe.FindAllStringSubmatch(page, -1) {
+		if strings.HasSuffix(match[0], ".manifest") {
+			continue
+		}
+
+		iso, edition := match[1], match[2]
+		url := mirror + iso
+		checksum := checksums[iso]
+		ch <- Config{
+			Release: release,
+			Edition: edition,
+			Arch:    arch,
+			ISO: []Source{
+				urlChecksumSource(url, checksum),
+			},
+		}
+	}
 }
