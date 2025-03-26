1df5bcee7989c9737217cf98428558e8632ec475
Liam <33645555+lj3954@users.noreply.github.com>
2025-03-26 09:31:03 -0700

    refactor: Separate out Archcraft config generation

--- internal/os/archcraft.go
+++ internal/os/archcraft.go
@@ -11,6 +11,7 @@ import (
 const (
 	archcraftMirror    = "https://sourceforge.net/projects/archcraft/files/"
 	archcraftReleaseRe = `"name":"v([^"]+)"`
+	archcraftUrlRe     = `"name":"archcraft-.*?-x86_64.iso".*?"download_url":"([^"]+)".*?"name":"archcraft-.*?-x86_64.iso.sha256sum".*?"download_url":"([^"]+)"`
 )
 
 type Archcraft struct{}
@@ -29,33 +30,46 @@ func (Archcraft) CreateConfigs(errs, csErrs chan<- Failure) ([]Config, error) {
 	if err != nil {
 		return nil, err
 	}
-	urlRe := regexp.MustCompile(`"name":"archcraft-.*?-x86_64.iso".*?"download_url":"([^"]+)".*?"name":"archcraft-.*?-x86_64.iso.sha256sum".*?"download_url":"([^"]+)"`)
+	urlRe := regexp.MustCompile(archcraftUrlRe)
 	ch, wg := getChannelsWith(numReleases)
 
 	for release := range releases {
 		mirror := fmt.Sprintf("%sv%s/", archcraftMirror, release)
 		go func() {
 			defer wg.Done()
-			page, err := web.CapturePage(mirror)
+			config, csErr, err := getArchcraftConfig(release, mirror, urlRe)
 			if err != nil {
 				errs <- Failure{Release: release, Error: err}
 				return
 			}
-			urls := urlRe.FindStringSubmatch(page)
-			if len(urls) == 3 {
-				checksum, err := cs.SingleWhitespace(urls[2])
-				if err != nil {
-					csErrs <- Failure{Release: release, Error: err}
-				}
-				ch <- Config{
-					Release: release,
-					ISO: []Source{
-						urlChecksumSource(urls[1], checksum),
-					},
-				}
+			if csErr != nil {
+				csErrs <- Failure{Release: release, Error: csErr}
 			}
+			ch <- *config
 		}()
 	}
 
 	return waitForConfigs(ch, wg), nil
+}
+
+func getArchcraftConfig(release string, mirror string, urlRe *regexp.Regexp) (config *Config, csErr error, e error) {
+	page, err := web.CapturePage(mirror)
+	if err != nil {
+		return nil, nil, err
+	}
+	urls := urlRe.FindStringSubmatch(page)
+	if len(urls) == 3 {
+		checksum, err := cs.SingleWhitespace(urls[2])
+		if err != nil {
+			csErr = err
+		}
+		config = &Config{
+			Release: release,
+			ISO: []Source{
+				urlChecksumSource(urls[1], checksum),
+			},
+		}
+	}
+
+	return
 }
