ca13b563412e29fb4ee5efca858b544bdcf8e4c4
Liam <33645555+lj3954@users.noreply.github.com>
2025-12-29 22:55:47 -0600

    refactor: Adapt zorin to new structure, add checksums

--- internal/os/zorin.go
+++ internal/os/zorin.go
@@ -1,6 +1,14 @@
 package os
 
-import "strings"
+import (
+	"errors"
+	"strconv"
+	"strings"
+
+	"github.com/quickemu-project/quickget_configs/internal/cs"
+	"github.com/quickemu-project/quickget_configs/internal/mirror"
+	"github.com/quickemu-project/quickget_configs/internal/web"
+)
 
 const (
 	zorinMirror        = "https://zrn.co/"
@@ -17,26 +25,68 @@ var Zorin = OS{
 }
 
 func createZorinConfigs(errs, csErrs chan<- Failure) ([]Config, error) {
-	releases, _, err := getBasicReleases(zorinReleaseMirror, zorinReleaseRe, -1)
+	c := mirror.HttpClient{}
+	head, err := c.ReadDir(zorinReleaseMirror)
 	if err != nil {
 		return nil, err
 	}
 
-	zorinEditions := []string{"core64", "lite64", "education64"}
+	zorinEditions := []string{"core", "lite", "education"}
 
-	var configs []Config
-	for release := range releases {
-		for _, edition := range zorinEditions {
-			url := zorinMirror + release + edition
-			configs = append(configs, Config{
-				Release: release,
-				Edition: strings.TrimSuffix(edition, "64"),
-				ISO: []Source{
-					urlSource(url),
-				},
-			})
+	ch, wg := getChannels()
+	for k, releaseDir := range head.SubDirs {
+		// Filter out non-numeric "releases" by parsing as a float
+		if _, err := strconv.ParseFloat(k, 64); err != nil {
+			continue
 		}
+		wg.Go(func() {
+			release := releaseDir.Name
+
+			checksums := make(map[string]string)
+			contents, err := releaseDir.Fetch(c)
+			// Directory contents are only used for checksums in this case
+			if err != nil {
+				csErrs <- Failure{Release: release, Error: err}
+			} else {
+				for k, f := range contents.Files {
+					k = strings.ToLower(k)
+					if strings.HasSuffix(k, ".txt") && strings.Contains(k, "sum") {
+						checksums, err = cs.Build(cs.Whitespace, f.URL)
+						if err != nil {
+							csErrs <- Failure{Release: release, Error: err}
+						} else {
+							break
+						}
+					}
+				}
+			}
+
+			for _, edition := range zorinEditions {
+				url := zorinMirror + release + edition + "64"
+
+				finalUrl, err := web.FinalRedirectUrl(url)
+				if err != nil {
+					errs <- Failure{Release: release, Error: err}
+					continue
+				}
+				fields := strings.Split(finalUrl, "/")
+				if len(fields) == 0 {
+					errs <- Failure{Release: release, Error: errors.New("final url has no fields")}
+					continue
+				}
+				filename := fields[len(fields)-1]
+				checksum := checksums[filename]
+
+				ch <- Config{
+					Release: release,
+					Edition: edition,
+					ISO: []Source{
+						webSource(url, checksum, "", filename),
+					},
+				}
+			}
+		})
 	}
 
-	return configs, nil
+	return waitForConfigs(ch, wg), nil
 }
