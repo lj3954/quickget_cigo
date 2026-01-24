5e91614a6ab93b0679015cd0242c4b0eb123353d
Liam <33645555+lj3954@users.noreply.github.com>
2026-01-23 18:20:29 -0800

    refactor: Adapt EasyOS to new mirror structure

--- internal/os/easyos.go
+++ internal/os/easyos.go
@@ -2,20 +2,19 @@ package os
 
 import (
 	"errors"
-	"regexp"
+	"maps"
 	"slices"
 	"strings"
 	"sync"
 
-	"github.com/hashicorp/go-version"
 	"github.com/quickemu-project/quickget_configs/internal/cs"
-	"github.com/quickemu-project/quickget_configs/internal/web"
+	"github.com/quickemu-project/quickget_configs/internal/mirror"
+	"github.com/quickemu-project/quickget_configs/internal/utils"
 	"github.com/quickemu-project/quickget_configs/pkg/quickgetdata"
 )
 
 const (
-	easyosMirror        = "https://distro.ibiblio.org/easyos/amd64/releases/"
-	easyosReleaseNameRe = `href="([a-z]+/)"`
+	easyosMirror = "https://distro.ibiblio.org/easyos/amd64/releases/"
 )
 
 var EasyOS = OS{
@@ -27,122 +26,109 @@ var EasyOS = OS{
 }
 
 func createEasyOSConfigs(errs, csErrs chan<- Failure) ([]Config, error) {
-	ch, wg := getChannels()
-	releases, err := getEasyOSReleases(5, wg, errs)
+	c := mirror.HttpClient{}
+	releases, err := getEasyOSReleases(errs, c)
 	if err != nil {
 		return nil, err
 	}
-	imgRe := regexp.MustCompile(`href="(easy-[0-9.]+-amd64.img(.gz)?)"`)
-	for _, relMirror := range releases {
-		release, mirror := relMirror.release, relMirror.mirror
+
+	slices.SortFunc(releases, func(a, b mirror.SubDirEntry) int {
+		return utils.SemverCompare(a.Name, b.Name)
+	})
+	releases = releases[max(len(releases)-3, 0):]
+
+	ch, wg := getChannels()
+	for _, d := range releases {
+		release := d.Name
 		wg.Go(func() {
-			page, err := web.CapturePage(mirror)
+			contents, err := d.Fetch()
 			if err != nil {
 				errs <- Failure{Release: release, Error: err}
 				return
 			}
-			checksum, err := cs.SingleWhitespace(mirror + "md5sum.txt")
-			if err != nil {
-				csErrs <- Failure{Release: release, Error: err}
+			var checksum string
+			if f, e := contents.Files["md5sum.txt"]; e {
+				checksum, err = cs.SingleWhitespace(f)
+				if err != nil {
+					csErrs <- Failure{Release: release, Error: err}
+				}
 			}
 
-			imgMatch := imgRe.FindStringSubmatch(page)
-			if imgMatch == nil {
-				errs <- Failure{Release: release, Error: errors.New("No image found")}
-				return
+			for k, f := range contents.Files {
+				if strings.HasSuffix(k, ".img") || strings.HasSuffix(k, ".img.gz") {
+					var archiveFormat quickgetdata.ArchiveFormat
+					if strings.HasSuffix(f.Name, ".img.gz") {
+						archiveFormat = quickgetdata.Gz
+					}
+					ch <- Config{
+						Release: release,
+						DiskImages: []Disk{
+							{
+								Source: webSource(f.URL.String(), checksum, archiveFormat, f.Name),
+								Format: quickgetdata.Raw,
+							},
+						},
+					}
+					return
+				}
 			}
-			url := mirror + imgMatch[1]
-			var archiveFormat quickgetdata.ArchiveFormat
-			if imgMatch[2] != "" {
-				archiveFormat = quickgetdata.Gz
-			}
-			ch <- Config{
-				Release: release,
-				DiskImages: []Disk{
-					{
-						Source: webSource(url, checksum, archiveFormat, ""),
-						Format: quickgetdata.Raw,
-					},
-				},
-			}
+
 		})
 	}
 
 	return waitForConfigs(ch, wg), nil
 }
 
-func getEasyOSReleases(maxReleases int, wg *sync.WaitGroup, errs chan<- Failure) ([]relMirror, error) {
-	releaseNames, _, err := getBasicReleases(easyosMirror, easyosReleaseNameRe, -1)
+func getEasyOSReleases(errs chan<- Failure, c mirror.Client) ([]mirror.SubDirEntry, error) {
+	contents, err := c.ReadDir(easyosMirror)
 	if err != nil {
 		return nil, err
 	}
-	subdirectoryRe := regexp.MustCompile(`href="([0-9]{4}/)"`)
-	releaseRe := regexp.MustCompile(`href="([0-9](?:\.[0-9]+)+)/"`)
 
-	ch := make(chan relMirror)
-	relMirrors := make([]relMirror, 0)
+	var wg sync.WaitGroup
+	ch := make(chan mirror.SubDirEntry)
+	var releases []mirror.SubDirEntry
 	go func() {
 		wg.Wait()
 		close(ch)
 	}()
 
-	for releaseName := range releaseNames {
-		mirror := easyosMirror + releaseName
+	for _, d := range contents.SubDirs {
 		wg.Go(func() {
-			page, err := web.CapturePage(mirror)
+			yearsDir, err := d.Fetch()
 			if err != nil {
-				errs <- Failure{Error: err}
+				errs <- Failure{Release: d.Name, Error: err}
 				return
 			}
-			matches := subdirectoryRe.FindAllStringSubmatch(page, -1)
-			for _, match := range matches {
-				mirror := mirror + match[1]
-				wg.Go(func() {
-					page, err := web.CapturePage(mirror)
-					if err != nil {
-						errs <- Failure{Error: err}
-						return
-					}
-					for _, match := range releaseRe.FindAllStringSubmatch(page, -1) {
-						ch <- relMirror{
-							release: match[1],
-							mirror:  mirror + match[1] + "/",
-						}
-					}
-				})
+
+			years := slices.Collect(maps.Values(yearsDir.SubDirs))
+			if len(years) == 0 {
+				errs <- Failure{Release: d.Name, Error: errors.New("no years found in directory")}
 			}
+			latestYear := slices.MaxFunc(years, func(a, b mirror.SubDirEntry) int {
+				return strings.Compare(a.Name, b.Name)
+			})
+
+			releasesDir, err := latestYear.Fetch()
+			if err != nil {
+				errs <- Failure{Release: d.Name, Error: err}
+				return
+			}
+
+			releases := slices.Collect(maps.Values(releasesDir.SubDirs))
+			if len(releases) == 0 {
+				errs <- Failure{Release: d.Name, Error: errors.New("no releases found in year directory")}
+			}
+			latestRelease := slices.MaxFunc(releases, func(a, b mirror.SubDirEntry) int {
+				return utils.SemverCompare(a.Name, b.Name)
+			})
+			ch <- latestRelease
 		})
 	}
 
-	for relMirror := range ch {
-		relMirrors = append(relMirrors, relMirror)
+	for release := range ch {
+		releases = append(releases, release)
 	}
 
-	releases := sortEasyOSReleases(relMirrors)
-	return releases[:min(len(releases), maxReleases)], nil
-}
-
-func sortEasyOSReleases(releases []relMirror) []relMirror {
-	slices.SortFunc(releases, func(a, b relMirror) int {
-		if aVer, err := version.NewVersion(a.release); err == nil {
-			if bVer, err := version.NewVersion(b.release); err == nil {
-				if cmp := bVer.Compare(aVer); cmp != 0 {
-					return cmp
-				}
-			}
-		}
-		return 0
-	})
-
-	return slices.CompactFunc(releases, func(a, b relMirror) bool {
-		aParts := strings.SplitN(a.release, ".", 3)
-		bParts := strings.SplitN(b.release, ".", 3)
-		return aParts[0] == bParts[0] && aParts[1] == bParts[1]
-	})
-
-}
-
-type relMirror struct {
-	release string
-	mirror  string
+	return releases, nil
 }
