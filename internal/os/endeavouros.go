6459125a22d80f55d886e3f9e0f232ab17bcca59
Liam <33645555+lj3954@users.noreply.github.com>
2026-01-30 16:40:01 -0800

    fix: Correct endeavouros checksum fetching. refactor mirror parsing

--- internal/os/endeavouros.go
+++ internal/os/endeavouros.go
@@ -2,9 +2,10 @@ package os
 
 import (
 	"regexp"
+	"strings"
 
 	"github.com/quickemu-project/quickget_configs/internal/cs"
-	"github.com/quickemu-project/quickget_configs/internal/web"
+	"github.com/quickemu-project/quickget_configs/internal/mirror"
 )
 
 const endeavourMirror = "https://mirror.alpix.eu/endeavouros/iso/"
@@ -18,26 +19,31 @@ var EndeavourOS = OS{
 }
 
 func createEndeavourOSConfigs(errs, csErrs chan<- Failure) ([]Config, error) {
-	page, err := web.CapturePage(endeavourMirror)
+	c := mirror.LegacyHttpClient{}
+	head, err := c.ReadDir(endeavourMirror)
 	if err != nil {
 		return nil, err
 	}
-	isoRe := regexp.MustCompile(`href="(EndeavourOS_[^\d]+(\d{4}.\d{2}.\d{2}).iso)"`)
-	matches := isoRe.FindAllStringSubmatch(page, -1)
+	isoRe := regexp.MustCompile(`^EndeavourOS_[^\d]+(\d{4}.\d{2}.\d{2}).iso$`)
 	ch, wg := getChannels()
-	for _, match := range matches {
-		release := match[2]
-		url := endeavourMirror + match[1]
-		checksumUrl := url + ".sha256sum"
+
+	for f, match := range head.FileMatches(isoRe) {
+		release := match[1]
 		wg.Go(func() {
-			checksum, err := cs.SingleWhitespace(checksumUrl)
-			if err != nil {
-				csErrs <- Failure{Release: release, Error: err}
+			cf, e := head.FindFile(func(f2 mirror.File) bool {
+				return strings.HasPrefix(f2.Name, f.Name) && strings.HasSuffix(f2.Name, "sum")
+			})
+			var checksum string
+			if e {
+				checksum, err = cs.SingleWhitespace(cf)
+				if err != nil {
+					csErrs <- Failure{Release: release, Error: err}
+				}
 			}
 			ch <- Config{
 				Release: release,
 				ISO: []Source{
-					urlChecksumSource(url, checksum),
+					webSource(f.URL.String(), checksum, "", f.Name),
 				},
 			}
 		})
