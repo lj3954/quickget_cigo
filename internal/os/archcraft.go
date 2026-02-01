7605b97385f094d518c26283e4b81e6317e8d1f9
Liam <33645555+lj3954@users.noreply.github.com>
2026-02-01 01:58:22 -0800

    refactor: Adapt Archcraft to new mirror structure

--- internal/os/archcraft.go
+++ internal/os/archcraft.go
@@ -1,16 +1,16 @@
 package os
 
 import (
-	"fmt"
-	"regexp"
+	"errors"
+	"strings"
 
 	"github.com/quickemu-project/quickget_configs/internal/cs"
-	"github.com/quickemu-project/quickget_configs/internal/web"
+	"github.com/quickemu-project/quickget_configs/internal/mirror"
+	"github.com/quickemu-project/quickget_configs/internal/utils"
 )
 
 const (
-	archcraftMirror    = "https://sourceforge.net/projects/archcraft/files/"
-	archcraftReleaseRe = `"name":"v([^"]+)"`
+	archcraftMirror = "https://sourceforge.net/projects/archcraft/files/"
 )
 
 var Archcraft = OS{
@@ -22,33 +22,48 @@ var Archcraft = OS{
 }
 
 func createArchcraftConfigs(errs, csErrs chan<- Failure) ([]Config, error) {
-	releases, _, err := getBasicReleases(archcraftMirror, archcraftReleaseRe, 3)
+	c := mirror.SourceForgeClient{}
+	head, err := c.ReadDir(archcraftMirror)
 	if err != nil {
 		return nil, err
 	}
-	urlRe := regexp.MustCompile(`"name":"archcraft-.*?-x86_64.iso".*?"download_url":"([^"]+)".*?"name":"archcraft-.*?-x86_64.iso.sha256sum".*?"download_url":"([^"]+)"`)
 	ch, wg := getChannels()
 
-	for release := range releases {
-		mirror := fmt.Sprintf("%sv%s/", archcraftMirror, release)
+	releases := head.NameSortedSubDirs(utils.SemverCompare)
+	releases = releases[max(len(releases)-3, 0):]
+
+	for _, d := range releases {
+		release := d.Name
 		wg.Go(func() {
-			page, err := web.CapturePage(mirror)
+			contents, err := d.Fetch()
 			if err != nil {
 				errs <- Failure{Release: release, Error: err}
 				return
 			}
-			urls := urlRe.FindStringSubmatch(page)
-			if len(urls) == 3 {
-				checksum, err := cs.SingleWhitespace(urls[2])
+			f, e := contents.FindFile(func(f mirror.File) bool {
+				return strings.HasSuffix(f.Name, ".iso")
+			})
+			if !e {
+				errs <- Failure{Release: release, Error: errors.New("could not find ISO in directory")}
+				return
+			}
+
+			var checksum string
+			cf, e := contents.FindFile(func(f2 mirror.File) bool {
+				return strings.HasPrefix(f2.Name, f.Name) && strings.HasSuffix(f2.Name, "sum")
+			})
+			if e {
+				checksum, err = cs.SingleWhitespace(cf)
 				if err != nil {
 					csErrs <- Failure{Release: release, Error: err}
 				}
-				ch <- Config{
-					Release: release,
-					ISO: []Source{
-						urlChecksumSource(urls[1], checksum),
-					},
-				}
+			}
+
+			ch <- Config{
+				Release: release,
+				ISO: []Source{
+					webSource(f.URL.String(), checksum, "", f.Name),
+				},
 			}
 		})
 	}
