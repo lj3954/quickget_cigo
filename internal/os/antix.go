548b175384e51c4add7e1d8d04534922a10a46d6
Liam <33645555+lj3954@users.noreply.github.com>
2026-02-17 11:59:34 -0800

    refactor: Adapt antix to new mirror pattern, recursively search editions

--- internal/os/antix.go
+++ internal/os/antix.go
@@ -1,14 +1,11 @@
 package os
 
 import (
-	"errors"
-	"fmt"
-	"iter"
 	"regexp"
 	"strings"
 
 	"github.com/quickemu-project/quickget_configs/internal/cs"
-	"github.com/quickemu-project/quickget_configs/internal/web"
+	"github.com/quickemu-project/quickget_configs/internal/mirror"
 )
 
 const (
@@ -25,80 +22,59 @@ var AntiX = OS{
 }
 
 func createAntiXConfigs(errs, csErrs chan<- Failure) ([]Config, error) {
-	releases, _, err := getBasicReleases(antiXMirror, antiXReleaseRe, 3)
+	c := mirror.SourceForgeClient{}
+	head, err := c.ReadDir(antiXMirror)
 	if err != nil {
 		return nil, err
 	}
-	ch, wg := getChannels()
-	isoRe := regexp.MustCompile(`"name":"(antiX-[0-9.]+(?:-runit)?(?:-[^_]+)?_x64-([^.]+).iso)".*?"download_url":"(.*?)"`)
 
-	var addConfigs = func(release string, mirror string, checksumUrl string, editionSuffix string) {
-		configs, csErr, err := createFinalAntiXConfigs(release, mirror, checksumUrl, isoRe, editionSuffix)
-		if err != nil {
-			errs <- Failure{Release: release, Error: err}
-			return
-		}
-		if csErr != nil {
-			csErrs <- Failure{Release: release, Error: csErr}
-		}
-		for config := range configs {
-			ch <- config
-		}
+	releases := head.ModifiedTimeSortedSubdirs()
+	releases = releases[max(len(releases)-4, 0):]
+
+	ch, wg := getChannels()
+	isoRe := regexp.MustCompile(`^antiX-[\d\.]+(?:-runit)?(?:-[^_]+)?_x64-([^.]+).iso$`)
+
+	var addConfigs func(release string, d mirror.SubDirEntry, edition string)
+	addConfigs = func(release string, d mirror.SubDirEntry, edition string) {
+		wg.Go(func() {
+			contents, err := d.Fetch()
+			if err != nil {
+				errs <- Failure{Release: release, Edition: edition, Error: err}
+				return
+			}
+
+			for k, dr := range contents.SubDirs {
+				suffix := "-" + d.Name
+				if prefix, e := strings.CutSuffix(k, suffix); e {
+					addConfigs(release, dr, prefix)
+				}
+			}
+
+			for f, match := range contents.FileMatches(isoRe) {
+				var checksum string
+				if cf, e := contents.Files[f.Name+".sha256"]; e {
+					checksum, err = cs.SingleWhitespace(cf)
+					if err != nil {
+						csErrs <- Failure{Release: release, Edition: edition, Error: err}
+					}
+				}
+				edition := match[1] + "-" + edition
+				ch <- Config{
+					Release: release,
+					Edition: edition,
+					ISO: []Source{
+						webSource(f.URL.String(), checksum, "", f.Name),
+					},
+				}
+			}
+
+		})
 	}
 
-	for release := range releases {
-		mirror := fmt.Sprintf("%santiX-%s/", antiXMirror, release)
-		checksumUrl := mirror + "README.txt/download"
-		wg.Go(func() {
-			addConfigs(release, mirror, checksumUrl, "-sysv")
-		})
-
-		runitMirror := fmt.Sprintf("%srunit-antiX-%s/", mirror, release)
-		runitChecksumUrl := runitMirror + "README2.txt/download"
-		wg.Go(func() {
-			addConfigs(release, runitMirror, runitChecksumUrl, "-runit")
-		})
+	for _, d := range releases {
+		release := strings.TrimPrefix(d.Name, "antiX-")
+		addConfigs(release, d, "sysv")
 	}
 
 	return waitForConfigs(ch, wg), nil
-}
-
-func createAntiXChecksums(url string) (map[string]string, error) {
-	page, err := web.CapturePage(url)
-	if err != nil {
-		return nil, err
-	}
-	data := strings.SplitN(page, "sha256:", 2)
-	if len(data) != 2 {
-		return nil, errors.New("Could not find antiX 'sha256' separator")
-	}
-	return cs.Whitespace.BuildWithData(data[1]), nil
-}
-
-func createFinalAntiXConfigs(release, url, checksumUrl string, isoRe *regexp.Regexp, editionSuffix string) (configs iter.Seq[Config], csErr error, err error) {
-	page, err := web.CapturePage(url)
-	if err != nil {
-		return
-	}
-	checksums, err := createAntiXChecksums(checksumUrl)
-	if err != nil {
-		csErr = err
-	}
-
-	return func(yield func(Config) bool) {
-		for _, match := range isoRe.FindAllStringSubmatch(page, -1) {
-			checksum, url := checksums[match[1]], match[3]
-			config := Config{
-				Release: release,
-				Edition: match[2] + editionSuffix,
-				ISO: []Source{
-					urlChecksumSource(url, checksum),
-				},
-			}
-
-			if !yield(config) {
-				return
-			}
-		}
-	}, csErr, nil
 }
